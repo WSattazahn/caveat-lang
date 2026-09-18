067ad0db0b1893bca4cd6873480c697c6f9382ce
WSattazahn <125209951+WSattazahn@users.noreply.github.com>
2026-09-18 08:33:21 +0800

    style: canonicalize CAVEAT-RS integration test

--- runtime/tests/caveat_rs.rs
+++ runtime/tests/caveat_rs.rs
@@ -1,5 +1,9 @@
 use caveat_runtime::caveat_rs::transpile;
-use std::{fs, process::Command, time::{SystemTime, UNIX_EPOCH}};
+use std::{
+    fs,
+    process::Command,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 #[test]
 fn example_transpiles_to_rust_that_rustc_accepts() {
@@ -10,10 +14,7 @@ fn example_transpiles_to_rust_that_rustc_accepts() {
         .duration_since(UNIX_EPOCH)
         .expect("clock should be after Unix epoch")
         .as_nanos();
-    let directory = std::env::temp_dir().join(format!(
-        "caveat-rs-{}-{nonce}",
-        std::process::id()
-    ));
+    let directory = std::env::temp_dir().join(format!("caveat-rs-{}-{nonce}", std::process::id()));
     fs::create_dir_all(&directory).expect("temporary directory should be created");
 
     let source_path = directory.join("generated.rs");
